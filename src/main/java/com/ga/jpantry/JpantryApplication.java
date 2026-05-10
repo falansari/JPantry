016a906a00cd598ae10d61e8a947bd01741103e1
Fatima Alansari <twoseasgamesstudio@gmail.com>
2026-05-10 19:24:04 +0300

    Added base project files closes #1

---
+++ src/main/java/com/ga/jpantry/JpantryApplication.java
@@ -0,0 +1,13 @@
+package com.ga.jpantry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpantryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpantryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ga/jpantry/JpantryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.jpantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpantryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
