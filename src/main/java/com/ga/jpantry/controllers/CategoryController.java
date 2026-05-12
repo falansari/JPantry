ba478c02400e5cfaa8004738246cc9575e467160
Fatima Alansari <twoseasgamesstudio@gmail.com>
2026-05-12 16:42:59 +0300

    Created category controller closes #5

---
+++ src/main/java/com/ga/jpantry/controllers/CategoryController.java
@@ -0,0 +1,86 @@
+package com.ga.jpantry.controllers;
+
+import com.ga.jpantry.exceptions.BadRequestException;
+import com.ga.jpantry.models.Category;
+import com.ga.jpantry.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    /**
+     * Find a category by its id or name.
+     * @param id Long
+     * @param name String
+     * @return Category
+     */
+    @GetMapping("")
+    public Category getCategory(@PathVariable(value = "id", required = false) Long id,
+                                @PathVariable(value = "name", required = false) String name) {
+        if (id != null) {
+            return categoryService.readById(id);
+        } else if (name != null) {
+            return categoryService.readByName(name);
+        } else {
+            throw new BadRequestException("Either an id or name must be provided to search for a category.");
+        }
+    }
+
+    /**
+     * Get all categories.
+     * @return CompletableFuture ArrayList Category
+     */
+    @GetMapping("/list")
+    public CompletableFuture<ArrayList<Category>> getAllCategories() {
+        return categoryService.readAll();
+    }
+
+    /**
+     * Create a new category.
+     * @param category Object {name (required) String, defaultExpiryPeriodDays (optional) int}
+     * @return Category
+     */
+    @PostMapping("/add")
+    public Category addCategory(Category category) {
+        return categoryService.create(category);
+    }
+
+    /**
+     * Update an existing category.
+     * @param category Object {id Long, name String, defaultExpiryPeriodDays int}
+     * @return Category updated record
+     */
+    @PostMapping("/edit")
+    public Category editCategory(Category category) {
+        return categoryService.updateById(category);
+    }
+
+    /**
+     * Delete a category.
+     * @param id Long
+     * @param name String
+     * @return boolean True if successful.
+     */
+    @PostMapping("/delete")
+    public boolean deleteCategory(@PathVariable(value = "id", required = false) Long id,
+                                @PathVariable(value = "name", required = false) String name) {
+        if (id != null) {
+            return categoryService.deleteById(id);
+        } else if (name != null) {
+            return categoryService.deleteByName(name);
+        } else {
+            throw new BadRequestException("Either an id or name must be provided to delete a category.");
+        }
+    }
+}
